297f5434ad1789179d3cf3c33a882d8544e19610
Janvier Habineza <janvierhabineza@Janviers-MacBook-Pro.local>
2024-04-07 17:05:00 +0200

    chore(app): add logger
    - add morgan
    - configure morgan as a logger
    
    [Finishes ]

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import Express from "express";
 import { Request, Response } from "express";
+import morgan from "morgan";
 
 const app = Express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({ message: "Welcome to our API"})
-})
+  res.status(200).json({ message: "Welcome to our API" });
+});
+app.use(morgan("combined"));
 
 app.listen(PORT, () => console.log("App is listening on port" + PORT));
