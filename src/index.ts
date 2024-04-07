7e0d6cf9a27347a2341064bf848a3193728c6a78
Heisjabo <jabojeanmarie5@gmail.com>
2024-04-07 16:45:41 +0200

    Add a welcome endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import Express from "express";
+import { Request, Response } from "express";
 
 const app = Express();
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Welcome to our API"})
+})
 
 app.listen(PORT, () => console.log("App is listening on port" + PORT));
